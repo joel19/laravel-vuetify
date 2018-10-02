abd0348b1cc64d7411db9163e68340b15ba73763
Joel Cano <joel.Alarcon1@gmail.com>
2018-10-01 23:26:51 -0500

    Framework settup

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,31 @@
+<template>
+<v-layout>
+    <v-flex xs12 sm6 d-flex>
+        <v-select
+          :items="items"
+          label="Standard"
+        ></v-select>
+    </v-flex>
+    <v-flex xs12 sm6 d-flex>
+        <v-select
+          :items="items"
+          label="Solo field"
+          solo
+        ></v-select>
+    </v-flex>
+</v-layout>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+    data () {
+        return {
+          items: ['Foo', 'Bar', 'Fizz', 'Buzz']
+        }
+    }
+
+    }
+</script>
